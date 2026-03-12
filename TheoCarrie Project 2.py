b8a0e6bc6ef46c77b9aa8c546976c2532091c274
theocarrie <tdgc2@kent.ac.uk>
2026-03-12 16:50:51 +0000

    attempting to make a turn based game i had to outline classes and "actions" that would be available to the player when they start the experience

---
+++ TheoCarrie Project 2.py
@@ -0,0 +1,16 @@
+element = ["Fire", "Water", "Earth", "Air"]
+action1 = ["Splosh", "Scorch", "Crunch", "Swoosh"]
+action2 = [" Waterfall", "Firewall", "Meteor", "Slice"]
+action3 = ["Tsunami", "Phoenix", "Fortress", "Cyclone"]
+
+print("LOADING...")
+print("...")
+
+
+def main():
+    print("Welcome to Elemenbrawl: choose your element: \n")
+    element = input ("-Water \n-Fire \n-Earth \n-Air \n")
+    if element == "Water":
+    elif element == "Fire":
+    elif element == "Earth":
+    elif element == "Air":
