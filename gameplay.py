d9851c169ba1f98eeef2cc692591cf1998a29098
theocarrie <tdgc2@kent.ac.uk>
2026-03-25 23:51:06 +0000

    im trying to make a simple system of choosing your attack and seeing the damage dealt (later adding effects to the initial damage)

--- gameplay.py
+++ gameplay.py
@@ -44,6 +44,7 @@ def elselect():
         elselect()
 
 def water():
+    selfel = water
     water = Element(name = "Water", health = 100)
     
     print("Water! Good choice, balanced and life sustaining, sure to wash away the competition! \nTHREE actions have been granted to you which will be availble to use on your turn. \n")
@@ -54,6 +55,7 @@ def water():
     loading()
 
 def fire():
+    selfel = fire
     fire = Element(name = "Fire", health = 100)
     print("Fire! Exellent choice, aggressive ""Scorch 'n burn"" playstyle, sure to incinerate the competition! \nTHREE actions have been granted to you which will be availble to use on your turn. \n")
     print("Scorch \nDMG: 40\nBURN: Target takes 10 DMG for 2 turns \n")
@@ -71,32 +73,68 @@ def gameplay():
          splosh.damage *= 1.5
          waterfall.damage *= 1.5
          tsunami.damage *= 1.5
+         opp = Element(name = "Fire Titan", health = 100)
+         print(f"ENEMY HEALTH {opp.health_max}")
+         fight()
      elif opel == "Water":
          print("ENEMY element is WATER, beware")
          crunch.damage *= 1.5
          meteor.damage *= 1.5
          fortress.damage *= 1.5
+         opp = Element(name = "Water Guardian", health = 100)
+         print(f"ENEMY HEALTH {opp.health_max}")
+         fight()
      elif opel == "Air":
          print("ENEMY element is AIR, beware")
          scorch.damage *= 1.5
          firewall.damage *= 1.5
          phoenix.damage *= 1.5
+         opp = Element(name = "Air Monk", health = 100)
+         print(f"ENEMY HEALTH {opp.health_max}")
+         fight()
      elif opel == "Earth":
          print("ENEMY element is EARTH, beware")
          swoosh.damage *= 1.5
          aslice.damage *= 1.5
-         cyclone.damage *= 1.5
+         cyclone.damage *= 1.5 
+         opp = Element(name = "Earth Shaker", health = 100)
+         print(f"ENEMY HEALTH {opp.health_max}")
+         fight()
     
+    
+
+def fight():
      print("It's your turn to attack! Choose a move!")
+     print(f"ENEMY HP:", {opp.health})
+     print(f"YOUR HP:", {opp.health})
      if elsel == "Water":
          movesel = input(f"1 -- {splosh.name} \n2 -- {waterfall.name} \n3 -- {tsunami.name} \n")
-        
+         if movesel == "1":
+             opp.health -= splosh.damage
+             print(f"ENEMY HP",{opp.health})
+         elif movesel == "2":
+            opp.health -= waterfall.damage
+            print(f"ENEMY HP",{opp.health})
+         elif movesel == "3":
+            opp.health -= tsunami.damage
+            print(f"ENEMY HP",{opp.health})
+       
      elif elsel == "Fire":
          movesel = input(f"1 -- {scorch.name} \n2 -- {firewall.name} \n3 -- {phoenix.name} \n")
+         if movesel == "1":
+             opp.health -= scorch.damage
+             print(f"ENEMY HP",{opp.health})
+         elif movesel == "2":
+            opp.health -= firewall.damage
+            print(f"ENEMY HP",{opp.health})
+         elif movesel == "3":
+            opp.health -= phoenix.damage
+            print(f"ENEMY HP",{opp.health})
      elif elsel == "Earth":
          movesel = input(f"1 -- {crunch.name} \n2 -- {meteor.name} \n3 -- {fortress.name} \n")
      elif elsel == "Air":
          movesel = input(f"1 -- {swoosh.name} \n2 -- {aslice.name} \n3 -- {cyclone.name} \n")
+
 
 
 
